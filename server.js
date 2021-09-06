12a850cc0c9c50ff10663ff228d3079852023c89
NayanSidhu <nsidh068@uottawa.ca>
2021-09-06 19:26:55 -0400

    bug fixes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +8,8 @@ app.use(express.json());
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Social-Network-API', {
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
